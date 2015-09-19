be1944cf41d7f1203a3f2000414a0517ae4a9ab7
M Taylor Elliott <mtaylorelliott@Ms-MacBook-Air.local>
2015-09-19 15:14:43 -0500

    yay

--- wp-content/themes/accelerate-theme-child/archive-services.php
+++ wp-content/themes/accelerate-theme-child/archive-services.php
@@ -11,34 +11,42 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-      <?php query_posts('posts_per_page=4&post_type=services'); ?>
-			<?php while ( have_posts() ) : the_post();
-              $image_right = get_field('image_right');
-              $image_left = get_field('image_left');
+<div id="primary" class="site-content">
+	<div id="content" role="main">
+    <?php query_posts('posts_per_page=4&post_type=services'); ?>
+
+    <?php while ( have_posts() ) : the_post();
+        $image_right = get_field('image_right');
+        $image_left = get_field('image_left');
+        ?>
+        <div id="container" class="clearfix">
+          <!-- moved the if above the div so it will not be created if we are doing image left -->
+          <?php if($image_right) { ?>
+            <div class="service-image-right">
+              <?php
+              $size = "full";
+              echo wp_get_attachment_image( $image_right, $size );
               ?>
-          <div class="service-image-right">
-            <?php
-    $size = "full"; ?>
-    <?php if($image_right) {
-    echo wp_get_attachment_image( $image_right, $size );
-    }
-    ?></div>
-    <div class="service-image-left"><?php
-$size = "full"; ?>
-<?php if($image_left) {
-echo wp_get_attachment_image( $image_left, $size );
-}
-?></div>
+            </div>
+          <?php } ?>
 
+          <!-- moved the if above the div so it will not be created if we are doing image right -->
+          <?php if($image_left) { ?>
+            <div class="service-image-left">
+              <?php
+              $size = "full";
+              echo wp_get_attachment_image( $image_left, $size );
+              ?>
+            </div>
+          <?php } ?>
 
-          <h4><?php the_title(); ?></h4>
+          <h4 class="titleclass"><?php the_title(); ?></h4>
           <p><?php the_content(); ?><p>
 
-			<?php endwhile; // end of the loop. ?>
+        </div> <!-- #container -->
+	   <?php endwhile; // end of the loop. ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+	</div><!-- #content -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
