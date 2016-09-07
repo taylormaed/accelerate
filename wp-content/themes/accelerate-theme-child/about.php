182d7a3192ee8fed6c04a6534d7cfd0a61e079f2
mtaylorelliott <tayls65@yahoo.com>
2016-09-07 13:01:26 -0500

    hopefully added services

--- wp-content/themes/accelerate-theme-child/about.php
+++ wp-content/themes/accelerate-theme-child/about.php
@@ -11,16 +11,16 @@
 
 get_header(); ?>
 
-<section class="home-page">
+<section class="about-page">
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='homepage-hero'>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
 			</div>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .container -->
-</section><!-- .home-page -->
+</section><!-- .about-page -->
+
 
 
 <p><a href="http://localhost:8888/accelerate/">Accelerate</a>
@@ -29,3 +29,40 @@ is a strategy and marketing agency located in the heart of NYC. Our goal is to b
 </div>
 </div>
 </section>
+
+<?php query_posts('posts_per_page=4&post_type=services'); ?>
+<?php while ( have_posts() ) : the_post();
+		$image_right = get_field('image_right');
+									$image_left = get_field('image_left');
+									$size = 'medium'?>
+<div id="container" class="clearfix">
+          <!-- moved the if above the div so it will not be created if we are doing image left -->
+          <?php if($image_right) { ?>
+            <div class="service-image-right">
+              <?php
+              $size = "full";
+              echo wp_get_attachment_image( $image_right, $size );
+              ?>
+            </div>
+          <?php } ?>
+
+          <!-- moved the if above the div so it will not be created if we are doing image right -->
+          <?php if($image_left) { ?>
+            <div class="service-image-left">
+              <?php
+              $size = "full";
+              echo wp_get_attachment_image( $image_left, $size );
+              ?>
+            </div>
+          <?php } ?>
+
+          <h4 class="titleclass"><?php the_title(); ?></h4>
+          <p><?php the_content(); ?><p>
+
+        </div> <!-- #container -->
+	   <?php endwhile; // end of the loop. ?>
+
+	</div><!-- #content -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
