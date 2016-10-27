3279e129a9f2cfc88e6d8f0e7096ed4101922afe
mtaylorelliott <tayls65@yahoo.com>
2016-10-27 15:34:01 -0500

    Ann fix 404

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -1,8 +1,6 @@
-<?php
+?php
 /**
  * This is the template for displaying the 404 page.
- *
- *
  *
  * @package WordPress
  * @subpackage Accelerate Marketing
@@ -10,23 +8,15 @@
  * Author:       Taylor Elliott
  * Author URI:   http://taylormaed.org
  * Template:     accelerate-theme
- *
- *
- *
- * @package WordPress
- * @subpackage Accelerate Marketing
- * @since Accelerate 1.1
  */
-
 get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-              <strong> <h2> <?php the_title(); ?></h2></strong>
-<br>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+
+			<h2>You appear to be lost!</h2>
+			
+			<h4><a href="<?php echo home_url(); ?>">Let's take you back home</a>. :)</h4>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
