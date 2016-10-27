7f8f0291383f7342b9b8ffae6c24461b9f343501
mtaylorelliott <tayls65@yahoo.com>
2016-10-27 13:38:38 -0500

    404

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This is the template for displaying the 404 page.
+ *
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate 1.1
+ * Author:       Taylor Elliott
+ * Author URI:   http://taylormaed.org
+ * Template:     accelerate-theme
+ *
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate 1.1
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+              <strong> <h2> <?php the_title(); ?></h2></strong>
+<br>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
