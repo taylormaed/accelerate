e2411e2e2cf83e1a5588dc8c8935d0ecad1dc3e3
mtaylorelliott <tayls65@yahoo.com>
2016-10-27 23:52:35 -0500

    still trying

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
 <section class="page_not_found">
 			<div class="page_not_found_wrap">
-			<h2>Hey! You seem to be lost</h2>
+			<h2>Hey! <br> You seem to be lost.</h2>
 			<h3>This way to go back <a href="<?php echo home_url(); ?>">home</a>. :)</h3>
 			<h4>Or <a href="http://localhost:8888/accelerate/contact_us/">contact us</a>!
 
