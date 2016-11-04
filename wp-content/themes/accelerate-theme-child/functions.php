05772e663ab22558b75df0be69814c3eec677570
mtaylorelliott <tayls65@yahoo.com>
2016-11-04 18:27:45 -0500

    updating functions file

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -64,8 +64,14 @@ function accelerate_theme_child_widget_init() {
         'name' =>__( 'Homepage sidebar, accelerate-theme-child'),
         'id' => 'sidebar-2',
         'description' => __( 'Appears on the static front page template', 'accelerate-theme-child'),
-        )
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+        ) );
+
 }
+
 
 add_action ( 'widgets_init',
     'accelerate_theme_child_widget_init');
