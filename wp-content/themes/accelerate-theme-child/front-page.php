f03b039dd31385f7e5668c3cbd333d8407cd540e
mtaylorelliott <tayls65@yahoo.com>
2016-11-15 22:04:08 -0600

    twitter module changes

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -60,14 +60,15 @@ get_header(); ?>
 	<?php wp_reset_query(); ?>
 		
 		</div>
-	</div>
-</section>
-
-<?php if ( is_active_sidebar('sidebar-2') ) : ?>
+	<?php if ( is_active_sidebar('sidebar-2') ) : ?>
 	<div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-2' ); ?>
 	</div>
 <?php endif; ?>
+	</div>
+</section>
+
+
 
 
 <?php get_footer(); ?>
